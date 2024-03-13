f5543edad92bd47bd5c0f456c3af8ac903a49f91
atulmishravelotio <atul.mishra@velotio.com>
2024-03-13 16:28:50 +0530

    read data from file and apply filters

--- query_manager/app/core/routes.py
+++ query_manager/app/core/routes.py
@@ -1,153 +1,69 @@
 import datetime
+import json
+import logging
 from typing import Annotated, Any, List, Optional
 
+import aiofiles
 from fastapi import APIRouter, Body
+import pandas as pd
 
-from app.core.schemas import Metric
-from app.core.types import Dimension
+from app.core.schemas import DimensionFilter, Metric
 
+logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/metrics", tags=["metrics"])
 
 
 @router.post("/values", response_model=list[Metric])
-async def read_metric(
+async def read_metric_values(
     metric_ids: List[int],
     start_date: Annotated[datetime.date, Body()],
     end_date: Annotated[datetime.date, Body()],
-    dimensions: Optional[List[Dimension]]=None
+    dimensions: Annotated[
+        Optional[List[DimensionFilter]],
+        Body(
+            examples=[
+                [
+                    {
+                        "dimension": "Geosegmentation",
+                        "slices": ["Rest of World", "other"],
+                    },
+                    {
+                        "dimension": "Creating Org",
+                        "slices": [],
+                    },
+                ]
+            ]
+        ),
+    ] = None,
 ) -> Any:
-    print("metric_ids: ", metric_ids)
-    print("start_date: ", start_date)
-    print("end_date: ", end_date)
-    print("dimensions: ", dimensions)
-    metrics = [
-        {
-            "id": 1,
-            "date": "1958-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Creating Org",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "1958-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Geosegmentation",
-            "slice": "Rest of World",
-        },
-        {
-            "id": 1,
-            "date": "1958-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Opportunity Source",
-            "slice": "Agency",
-        },
-        {
-            "id": 1,
-            "date": "1958-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Owning Org",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "1958-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Probability to Close Tier",
-            "slice": "High",
-        },
-        {
-            "id": 1,
-            "date": "1999-10-30",
-            "name": "Count of Closed Opportunities",
-            "value": 3,
-            "dimension": "Creating Org",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "1999-10-30",
-            "name": "Count of Closed Opportunities",
-            "value": 3,
-            "dimension": "Geosegmentation",
-            "slice": "Americas",
-        },
-        {
-            "id": 1,
-            "date": "1999-10-30",
-            "name": "Count of Closed Opportunities",
-            "value": 3,
-            "dimension": "Opportunity Source",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "1999-10-30",
-            "name": "Count of Closed Opportunities",
-            "value": 3,
-            "dimension": "Owning Org",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "1999-10-30",
-            "name": "Count of Closed Opportunities",
-            "value": 3,
-            "dimension": "Probability to Close Tier",
-            "slice": "Low",
-        },
-        {
-            "id": 1,
-            "date": "2000-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Creating Org",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "2000-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Geosegmentation",
-            "slice": "Americas",
-        },
-        {
-            "id": 1,
-            "date": "2000-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Opportunity Source",
-            "slice": "Agency",
-        },
-        {
-            "id": 1,
-            "date": "2000-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Owning Org",
-            "slice": "Other",
-        },
-        {
-            "id": 1,
-            "date": "2000-01-04",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Probability to Close Tier",
-            "slice": "Low",
-        },
-        {
-            "id": 1,
-            "date": "2006-04-28",
-            "name": "Count of Closed Opportunities",
-            "value": 1,
-            "dimension": "Creating Org",
-            "slice": "Other",
-        },
-    ]
-    return metrics
+    logger.debug("metric_ids: ", metric_ids)
+    logger.debug("start_date: ", start_date)
+    logger.debug("end_date: ", end_date)
+    logger.debug("dimensions: ", dimensions)
+
+    async with aiofiles.open("app/core/data/metric_values.json") as f:
+        contents = await f.read()
+    metrics = json.loads(contents)
+
+    # prepare df and filter
+    metrics_df = pd.DataFrame(metrics)
+
+    metrics_df["date"] = pd.to_datetime(metrics_df["date"]).dt.date
+
+    # filter by metric ids
+    metrics_df = metrics_df[metrics_df["id"].isin(metric_ids)]
+    metrics_df = metrics_df[(metrics_df["date"] >= start_date) & (metrics_df["date"] <= end_date)]
+
+    # filter by dimensions
+    if not dimensions:
+        return metrics_df.to_dict(orient="records")
+
+    result_df = pd.DataFrame()
+    for dimension in dimensions:
+        slices = dimension.slices
+        _df = metrics_df[metrics_df["dimension"] == dimension.dimension]
+        if slices:
+            _df = metrics_df[metrics_df["slice"].isin(slices)]
+        result_df = pd.concat([result_df, _df])
+
+    return result_df.to_dict(orient="records")
--- query_manager/app/core/schemas.py
+++ query_manager/app/core/schemas.py
@@ -1,12 +1,18 @@
 import datetime
+from typing import List, Optional
 
 from app.utilities.schema import BaseModel
 
 
 class Metric(BaseModel):
     id: int
-    date: str
+    date: datetime.date
     name: str
     dimension: str
     slice: str
     value: int
+
+
+class DimensionFilter(BaseModel):
+    dimension: str
+    slices: Optional[List[str]] = None
