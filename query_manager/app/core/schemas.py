bfdd061f955cd33ce0ee10c75aa7ee65ce9f635d
atulmishravelotio <53421398+atulmishravelotio@users.noreply.github.com>
2024-03-13 18:19:46 +0530

    LEV-135 add metrics values mock api (#9)

--- query_manager/app/core/schemas.py
+++ query_manager/app/core/schemas.py
@@ -1,7 +1,18 @@
+import datetime
+from typing import List, Optional
+
 from app.utilities.schema import BaseModel
 
 
 class Metric(BaseModel):
     id: int
+    date: datetime.date
     name: str
-    # more fields to be added
+    dimension: str
+    slice: str
+    value: int
+
+
+class DimensionFilter(BaseModel):
+    dimension: str
+    slices: Optional[List[str]] = None
