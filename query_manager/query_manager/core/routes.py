3532fd7f607d93c1096487f0964caa4644ff62d0
Suraj Patil <suraj.patil@velotio.com>
2024-04-08 15:58:57 +0530

    query manager api changes, exception handling

--- query_manager/query_manager/core/routes.py
+++ query_manager/query_manager/core/routes.py
@@ -1,10 +1,11 @@
 import logging
+from datetime import date
 from typing import Annotated
 
 from fastapi import (
     APIRouter,
     Body,
-    HTTPException,
+    Query,
     Request,
 )
 
@@ -13,10 +14,12 @@ from query_manager.core.schemas import (
     Dimension,
     DimensionDetail,
     MetricDetail,
-    MetricList,
-    MetricValueResponse,
+    MetricListResponse,
+    MetricValue,
+    MetricValuesResponse,
     Target,
 )
+from query_manager.exceptions import MetricNotFoundError
 from query_manager.services.s3 import NoSuchKeyError
 from query_manager.utilities.enums import OutputFormat
 
@@ -24,12 +27,15 @@ logger = logging.getLogger(__name__)
 router = APIRouter(prefix="")
 
 
-@router.get("/metrics", response_model=list[MetricList], tags=["metrics"])
-async def list_metrics(client: QueryClientDep):
+@router.get("/metrics", response_model=MetricListResponse, tags=["metrics"])
+async def list_metrics(
+    client: QueryClientDep, metric_ids: Annotated[list[str] | None, Query(description="List of metric IDs")] = None
+):
     """
     Retrieve a list of metrics.
     """
-    return await client.list_metrics()
+    results = await client.list_metrics(metric_ids=metric_ids)
+    return {"results": results}
 
 
 @router.get("/metrics/{metric_id}", response_model=MetricDetail, tags=["metrics"])
@@ -65,14 +71,31 @@ async def get_dimension_members(dimension_id: str, client: QueryClientDep):
 
 
 # Value APIs
-@router.post("/metrics/{metric_id}/values", response_model=MetricValueResponse, tags=["metrics"])
+@router.get("/metrics/{metric_id}/value", response_model=MetricValue, tags=["metrics"])
+async def get_metric_value(
+    client: QueryClientDep,
+    metric_id: str,
+    start_date: date | None = None,
+    end_date: date | None = None,
+):
+    """
+    Retrieve the aggregated value of a metric within a date range.
+    """
+    try:
+        res = await client.get_metric_value(metric_id, start_date, end_date)
+    except NoSuchKeyError as e:
+        raise MetricNotFoundError(metric_id) from e
+    return res
+
+
+@router.post("/metrics/{metric_id}/values", response_model=MetricValuesResponse, tags=["metrics"])
 async def get_metric_values(
     request: Request,
     client: QueryClientDep,
     parquet_service: ParquetServiceDep,
     metric_id: str,
-    start_date: Annotated[str, Body(description="The start date of the date range.")],
-    end_date: Annotated[str, Body(description="The end date of the date range.")],
+    start_date: Annotated[date, Body(description="The start date of the date range.")],
+    end_date: Annotated[date, Body(description="The end date of the date range.")],
     dimensions: Annotated[
         list[str] | None,
         Body(description="Can be either 'all' or list of dimension ids."),
@@ -87,7 +110,7 @@ async def get_metric_values(
     try:
         res = await client.get_metric_values(metric_id, start_date, end_date, dimensions=dimensions)
     except NoSuchKeyError as e:
-        raise HTTPException(status_code=404, detail=f"Metric '{metric_id}' not found.") from e
+        raise MetricNotFoundError(metric_id) from e
 
     if output_format == OutputFormat.PARQUET:
         parquet_url = await parquet_service.convert_and_upload(res, metric_id, request_id)
@@ -108,5 +131,5 @@ async def get_metric_targets(
     try:
         res = await client.get_metric_targets(metric_id, start_date=start_date, end_date=end_date)
     except NoSuchKeyError as e:
-        raise HTTPException(status_code=404, detail=f"Metric '{metric_id}' not found.") from e
+        raise MetricNotFoundError(metric_id) from e
     return res
---
+++ query_manager/query_manager/exceptions.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+from fastapi import HTTPException, Request
+from starlette.responses import JSONResponse
+from starlette.status import HTTP_404_NOT_FOUND
+
+
+class ErrorCode(str, Enum):
+    """
+    An enumeration of error codes.
+    """
+
+    METRIC_VALUE_NOT_FOUND = "metric_value_not_found"
+    METRIC_NOT_FOUND = "metric_not_found"
+
+
+class QueryManagerError(HTTPException):
+    def __init__(self, status_code: int, code: ErrorCode, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
+        self.code = code
+
+
+class MetricValueNotFoundError(QueryManagerError):
+    def __init__(self, metric_id: str):
+        self.metric_id = metric_id
+        detail = f"Value for metric '{metric_id}' not found."
+        super().__init__(status_code=HTTP_404_NOT_FOUND, detail=detail, code=ErrorCode.METRIC_VALUE_NOT_FOUND)
+
+
+class MetricNotFoundError(QueryManagerError):
+    def __init__(self, metric_id: str):
+        self.metric_id = metric_id
+        detail = f"Metric '{metric_id}' not found."
+        super().__init__(status_code=HTTP_404_NOT_FOUND, detail=detail, code=ErrorCode.METRIC_NOT_FOUND)
+
+
+def add_exception_handlers(app):
+    @app.exception_handler(QueryManagerError)
+    async def query_manager_exception_handler(request: Request, exc: QueryManagerError):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"error": exc.code, "detail": exc.detail},
+        )
