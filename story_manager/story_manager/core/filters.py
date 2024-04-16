de86a36d5f7e78b517bd833e73207ffafb0f63d6
Suraj Patil <suraj.patil@velotio.com>
2024-04-16 21:28:51 +0530

    minor fix

--- story_manager/story_manager/core/filters.py
+++ story_manager/story_manager/core/filters.py
@@ -6,7 +6,7 @@ from story_manager.core.models import Story
 
 
 class StoryFilter(BaseFilter[Story]):
-    metric_id: str | None = FilterField(Story.metric_id.prop.columns, operator="eq", default=None)  # type: ignore
+    metric_id: str | None = FilterField(Story.metric_id, operator="eq", default=None)  # type: ignore
     created_at_start: datetime | None = FilterField(Story.created_at, operator="ge", default=None)  # type: ignore
     created_at_end: datetime | None = FilterField(Story.created_at, operator="le", default=None)  # type: ignore
     genre: StoryGenre | None = FilterField(Story.genre, operator="eq", default=None)  # type: ignore
