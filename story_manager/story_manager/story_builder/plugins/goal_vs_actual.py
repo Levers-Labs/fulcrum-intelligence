70f9ee1b0ea9bdd3c21c31641ff9470f5ae0bb1d
sadaf-vl <167412115+sadaf-vl@users.noreply.github.com>
2024-05-29 14:33:42 +0530

    [LEV-291] -- goal vs actual story impl (#54)

---
+++ story_manager/story_manager/story_builder/plugins/goal_vs_actual.py
@@ -0,0 +1,124 @@
+import logging
+
+import pandas as pd
+
+from commons.models.enums import Granularity
+from story_manager.core.enums import (
+    Direction,
+    StoryGenre,
+    StoryGroup,
+    StoryType,
+)
+from story_manager.story_builder import StoryBuilderBase
+
+logger = logging.getLogger(__name__)
+
+
+class GoalVsActualStoryBuilder(StoryBuilderBase):
+    genre = StoryGenre.PERFORMANCE
+    group = StoryGroup.GOAL_VS_ACTUAL
+    supported_grains = [Granularity.DAY, Granularity.WEEK, Granularity.MONTH]
+
+    async def generate_stories(self, metric_id: str, grain: Granularity) -> list[dict]:
+        """
+        Generate trends stories for the given metric and grain.
+        Analyze the process control response DataFrame to identify trends.
+
+        Input:
+        The input DataFrame should contain the following columns:
+        - date: The date of the metric data point.
+        - value: The value of the metric.
+        - target: The target for the date.
+
+        Logic:
+        - A time series of metric and target values is constructed for the grain and span relevant to the digest.
+        - The most recent period’s actual is compared to the corresponding target.
+        - If a target is not available for the current period, no story is created.
+        - If the actual is at or above the target, an On Track story is created. Else an Off Track story.
+
+        Output:
+        A list of stories containing metadata for each identified trend.
+
+        :param metric_id: The metric ID for which trends stories are generated.
+        :param grain: The grain of the time series data.
+        :return: A list containing a goal_vs_actual story dictionary.
+        """
+        stories: list[dict] = []
+
+        # get metric details
+        metric = await self.query_service.get_metric(metric_id)
+
+        # find the start and end date for the input time series data
+        start_date, end_date = self._get_input_time_range(grain)
+
+        # get time series data with targets
+        df = await self._get_time_series_data_with_targets(metric_id, grain, start_date, end_date)
+
+        # validate time series data has minimum required data points
+        time_durations = self.get_time_durations(grain)
+        if len(df) < time_durations["min"]:
+            logging.warning(
+                "Discarding story generation for metric '%s' with grain '%s' due to insufficient data", metric_id, grain
+            )
+            return []
+
+        # get growth rate for the series
+        df["growth_rate"] = self.analysis_manager.calculate_growth_rates_of_series(df["value"])
+        df["growth_rate"] = df["growth_rate"].fillna(value=0)
+
+        # get the most recent date for the period
+        latest_date, _ = self._get_current_period_range(grain)
+
+        # data for the most recent date
+        ref_data = df[df["date"] == pd.to_datetime(latest_date)]
+        # validate if data exists for the date
+        if ref_data.empty:
+            logging.warning(
+                "Discarding story generation for metric '%s' with grain '%s' due to no data for the most "
+                "recent period date '%s'",
+                metric_id,
+                grain,
+                latest_date,
+            )
+            return []
+
+        value = ref_data.iloc[-1]["value"].item()
+        growth = ref_data.iloc[-1]["growth_rate"].item()
+        target = ref_data.iloc[-1]["target"]
+
+        # validate if target exist, exit if no target
+        if pd.isna(target):
+            logging.warning(
+                "Discarding story generation for metric '%s' with grain '%s' due to no target for the most "
+                "recent period date '%s'",
+                metric_id,
+                grain,
+                latest_date,
+            )
+            return []
+
+        target = target.item()
+        if value >= target:
+            story_type = StoryType.ON_TRACK
+            direction = Direction.UP.value
+        else:
+            story_type = StoryType.OFF_TRACK
+            direction = Direction.DOWN.value
+
+        # calculate deviation % of value from the target
+        deviation = self.analysis_manager.calculate_percentage_difference(value, target)
+        story_details = self.prepare_story_dict(
+            story_type,
+            grain=grain,
+            metric=metric,
+            df=df,
+            current_value=value,
+            direction=direction,
+            current_growth=growth,
+            target=target,
+            deviation=abs(deviation),
+            duration=len(df),
+        )
+        stories.append(story_details)
+        logger.info(f"Stories generated for metric '{metric_id}', story details: {story_details}")
+        return stories
---
+++ story_manager/tests/unit/story_builder/test_plugins/test_goal_vs_actual.py
@@ -0,0 +1,107 @@
+from datetime import date
+from unittest.mock import AsyncMock
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from commons.models.enums import Granularity
+from story_manager.core.enums import StoryType
+from story_manager.story_builder.plugins.goal_vs_actual import GoalVsActualStoryBuilder
+
+start_date = date(2023, 4, 7)
+number_of_data_points = 90
+
+
+@pytest.fixture
+def goal_vs_actual_story_builder(
+    mock_query_service, mock_analysis_service, mock_analysis_manager, mock_db_session, metric_values
+):
+    mock_query_service.get_metric = AsyncMock(return_value={"id": "metric_1", "label": "Metric 1"})
+    return GoalVsActualStoryBuilder(mock_query_service, mock_analysis_service, mock_analysis_manager, mock_db_session)
+
+
+@pytest.mark.asyncio
+async def test_generate_goal_vs_actual_stories_no_min_data_points(goal_vs_actual_story_builder, targets_df):
+    # Prepare
+    goal_vs_actual_story_builder._get_time_series_data_with_targets = AsyncMock(return_value=targets_df)
+
+    # Act
+    result = await goal_vs_actual_story_builder.generate_stories("metric_1", Granularity.DAY)
+
+    # Assert
+    assert len(result) == 0
+
+
+@pytest.mark.asyncio
+async def test_generate_goal_vs_actual_stories_on_track(goal_vs_actual_story_builder, targets_df):
+    latest_date, _ = goal_vs_actual_story_builder._get_current_period_range(Granularity.DAY)
+
+    targets_df = targets_df.copy()
+    on_track_data = {
+        "value": 100,
+        "target": 50,
+        "date": pd.to_datetime(latest_date),
+    }
+    targets_df.loc[len(targets_df)] = on_track_data
+    goal_vs_actual_story_builder._get_time_series_data_with_targets = AsyncMock(return_value=targets_df)
+
+    result = await goal_vs_actual_story_builder.generate_stories("metric_1", Granularity.DAY)
+
+    story = result[0]
+    assert story["story_type"] == StoryType.ON_TRACK
+
+
+@pytest.mark.asyncio
+async def test_generate_goal_vs_actual_stories_off_track(goal_vs_actual_story_builder, targets_df):
+    latest_date, _ = goal_vs_actual_story_builder._get_current_period_range(Granularity.DAY)
+
+    targets_df = targets_df.copy()
+    on_track_data = {
+        "value": 0,
+        "target": 50,
+        "date": pd.to_datetime(latest_date),
+    }
+    targets_df.loc[len(targets_df)] = on_track_data
+    goal_vs_actual_story_builder._get_time_series_data_with_targets = AsyncMock(return_value=targets_df)
+
+    result = await goal_vs_actual_story_builder.generate_stories("metric_1", Granularity.DAY)
+
+    story = result[0]
+    assert story["story_type"] == StoryType.OFF_TRACK
+
+
+@pytest.mark.asyncio
+async def test_generate_goal_vs_actual_stories_none_target(goal_vs_actual_story_builder, targets_df):
+    latest_date, _ = goal_vs_actual_story_builder._get_current_period_range(Granularity.DAY)
+    latest_date = latest_date.strftime("%Y-%m-%d")
+    targets_df = targets_df.copy()
+    on_track_data = {
+        "value": 0,
+        "target": None,
+        "date": latest_date,
+    }
+    targets_df.loc[len(targets_df)] = on_track_data
+    goal_vs_actual_story_builder._get_time_series_data_with_targets = AsyncMock(return_value=targets_df)
+
+    result = await goal_vs_actual_story_builder.generate_stories("metric_1", Granularity.DAY)
+
+    assert len(result) == 0
+
+
+@pytest.mark.asyncio
+async def test_generate_goal_vs_actual_stories_nan_target(goal_vs_actual_story_builder, targets_df):
+    latest_date, _ = goal_vs_actual_story_builder._get_current_period_range(Granularity.DAY)
+
+    targets_df = targets_df.copy()
+    on_track_data = {
+        "value": 0,
+        "target": np.NaN,
+        "date": pd.to_datetime(latest_date),
+    }
+    targets_df.loc[len(targets_df)] = on_track_data
+    goal_vs_actual_story_builder._get_time_series_data_with_targets = AsyncMock(return_value=targets_df)
+
+    result = await goal_vs_actual_story_builder.generate_stories("metric_1", Granularity.DAY)
+
+    assert len(result) == 0
