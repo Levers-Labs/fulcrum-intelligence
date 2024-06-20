125cda878a8c09d013d7d070d32ca004761446ed
sadaf-vl <sadaf.shaikh@velotio.com>
2024-06-20 18:52:28 +0530

    type ignore

--- story_manager/story_manager/core/filters.py
+++ story_manager/story_manager/core/filters.py
@@ -14,15 +14,15 @@ from story_manager.core.models import Story
 
 
 class StoryFilter(BaseFilter[Story]):
-    metric_ids: list[str] | None = FilterField(Story.metric_id, operator="in", default=None)  # noqa
-    story_date_start: datetime | None = FilterField(Story.story_date, operator="ge", default=None)  # noqa
-    story_date_end: datetime | None = FilterField(Story.story_date, operator="le", default=None)  # noqa
-    genres: list[StoryGenre] | None = FilterField(Story.genre, operator="in", default=None)  # noqa
-    story_types: list[StoryType] | None = FilterField(Story.story_type, operator="in", default=None)  # noqa
-    story_groups: list[StoryGroup] | None = FilterField(Story.story_group, operator="in", default=None)  # noqa
-    grains: list[Granularity] | None = FilterField(Story.grain, operator="in", default=None)  # noqa
-    digest: Digest | None  # noqa
-    section: Section | None  # noqa
+    metric_ids: list[str] | None = FilterField(Story.metric_id, operator="in", default=None)  # type: ignore
+    story_date_start: datetime | None = FilterField(Story.story_date, operator="ge", default=None)  # type: ignore
+    story_date_end: datetime | None = FilterField(Story.story_date, operator="le", default=None)  # type: ignore
+    genres: list[StoryGenre] | None = FilterField(Story.genre, operator="in", default=None)  # type: ignore
+    story_types: list[StoryType] | None = FilterField(Story.story_type, operator="in", default=None)  # type: ignore
+    story_groups: list[StoryGroup] | None = FilterField(Story.story_group, operator="in", default=None)  # type: ignore
+    grains: list[Granularity] | None = FilterField(Story.grain, operator="in", default=None)  # type: ignore
+    digest: Digest | None  # type: ignore
+    section: Section | None  # type: ignore
 
     def apply_predefined_filters(self) -> None:
         if self.digest and self.section:
