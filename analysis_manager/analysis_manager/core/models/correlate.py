7fcb026abfa43b6244fd2627b34cbe31c7942d4d
sadaf-vl <167412115+sadaf-vl@users.noreply.github.com>
2024-06-21 16:48:31 +0530

    Schema for Analysis Manager  (#71)

--- analysis_manager/analysis_manager/core/models/correlate.py
+++ analysis_manager/analysis_manager/core/models/correlate.py
@@ -2,16 +2,19 @@ from datetime import date
 
 from sqlmodel import Field
 
-from commons.db.models import BaseSQLModel, BaseTimeStampedModel
+from analysis_manager.core.models.base_model import AnalysisSchemaBaseModel
+from commons.db.models import BaseSQLModel
 
 
 class CorrelateBase(BaseSQLModel):
+
     metric_id_1: str = Field(max_length=255, index=True)
     metric_id_2: str = Field(max_length=255, index=True)
     correlation_coefficient: float
 
 
-class Correlate(BaseTimeStampedModel, CorrelateBase, table=True):  # type: ignore
+class Correlate(AnalysisSchemaBaseModel, CorrelateBase, table=True):  # type: ignore
+
     start_date: date
     end_date: date
 
