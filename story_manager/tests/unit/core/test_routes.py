98ebd61e15603a0b798981550a6ebe2be78611e8
sadaf-vl <sadaf.shaikh@velotio.com>
2024-07-30 16:25:20 +0530

    fix testcases

--- story_manager/tests/unit/core/test_routes.py
+++ story_manager/tests/unit/core/test_routes.py
@@ -22,12 +22,12 @@ async def test_get_story_group_meta_success(mocker, client):
     mocker.patch.object(StoryFactory, "get_story_builder", return_value=MockStoryBuilder)
 
     # Get the story group metadata
-    response = client.get("/v1/stories/?story_groups=GROWTH_RATES")
+    response = client.get(f"/v1/stories/groups/{StoryGroup.TREND_CHANGES.value}")
 
     # Check the response
     assert response.status_code == 200
     result = response.json()
-    assert result["group"] == StoryGroup.GROWTH_RATES.value
+    assert result["group"] == StoryGroup.TREND_CHANGES.value
     assert result["grains"] == [Granularity.DAY.value, Granularity.WEEK.value]
 
 
@@ -110,6 +110,66 @@ async def test_get_stories(db_session, client):
             "current_growth}}% and down from the {{reference_growth}}% average over the past {{"
             "reference_period_days}} {{grain}}s.",
         ),
+        Story(
+            genre=StoryGenre.TRENDS,
+            story_group=StoryGroup.TREND_EXCEPTIONS,
+            grain=Granularity.MONTH,
+            story_date=datetime(2020, 1, 1),
+            story_type=StoryType.SPIKE,
+            metric_id="NewBizDeals",
+            title="d/d growth is slowing down",
+            title_template="{{pop}} growth is slowing down",
+            detail="The d/d growth rate for NewBizDeals is slowing down. It is currently 10% and down from the 15% "
+            "average over the past 5 days.",
+            detail_template="The {{pop}} growth rate for {{metric.label}} is slowing down. It is currently {{"
+            "current_growth}}% and down from the {{reference_growth}}% average over the past {{"
+            "reference_period_days}} {{grain}}s.",
+        ),
+        Story(
+            genre=StoryGenre.PERFORMANCE,
+            story_group=StoryGroup.LIKELY_STATUS,
+            grain=Granularity.MONTH,
+            story_date=datetime(2020, 1, 1),
+            story_type=StoryType.LIKELY_OFF_TRACK,
+            metric_id="NewBizDeals",
+            title="d/d growth is slowing down",
+            title_template="{{pop}} growth is slowing down",
+            detail="The d/d growth rate for NewBizDeals is slowing down. It is currently 10% and down from the 15% "
+            "average over the past 5 days.",
+            detail_template="The {{pop}} growth rate for {{metric.label}} is slowing down. It is currently {{"
+            "current_growth}}% and down from the {{reference_growth}}% average over the past {{"
+            "reference_period_days}} {{grain}}s.",
+        ),
+        Story(
+            genre=StoryGenre.PERFORMANCE,
+            story_group=StoryGroup.STATUS_CHANGE,
+            grain=Granularity.MONTH,
+            story_date=datetime(2020, 1, 1),
+            story_type=StoryType.IMPROVING_STATUS,
+            metric_id="NewBizDeals",
+            title="d/d growth is slowing down",
+            title_template="{{pop}} growth is slowing down",
+            detail="The d/d growth rate for NewBizDeals is slowing down. It is currently 10% and down from the 15% "
+            "average over the past 5 days.",
+            detail_template="The {{pop}} growth rate for {{metric.label}} is slowing down. It is currently {{"
+            "current_growth}}% and down from the {{reference_growth}}% average over the past {{"
+            "reference_period_days}} {{grain}}s.",
+        ),
+        Story(
+            genre=StoryGenre.ROOT_CAUSES,
+            story_group=StoryGroup.SEGMENT_DRIFT,
+            grain=Granularity.MONTH,
+            story_date=datetime(2020, 1, 1),
+            story_type=StoryType.SHRINKING_SEGMENT,
+            metric_id="NewBizDeals",
+            title="d/d growth is slowing down",
+            title_template="{{pop}} growth is slowing down",
+            detail="The d/d growth rate for NewBizDeals is slowing down. It is currently 10% and down from the 15% "
+            "average over the past 5 days.",
+            detail_template="The {{pop}} growth rate for {{metric.label}} is slowing down. It is currently {{"
+            "current_growth}}% and down from the {{reference_growth}}% average over the past {{"
+            "reference_period_days}} {{grain}}s.",
+        ),
     ]
     db_session.add_all(stories)
     db_session.commit()
@@ -147,11 +207,11 @@ async def test_get_stories(db_session, client):
     response = client.get("/v1/stories?story_groups=GROWTH_RATES&story_types=ACCELERATING_GROWTH")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["count"] == 3
+    assert data["count"] == 2
     assert data["results"][0]["genre"] == StoryGenre.GROWTH.value
     assert data["results"][0]["story_group"] == StoryGroup.GROWTH_RATES.value
     assert data["results"][0]["grain"] == Granularity.DAY.value
-    assert data["results"][0]["metric_id"] == "CAC"
+    assert data["results"][0]["metric_id"] == "NewMRR"
 
     # Test Multiple story types based filtering
     response = client.get("/v1/stories?story_types=NEW_UPWARD_TREND&story_types=ACCELERATING_GROWTH")
@@ -169,19 +229,19 @@ async def test_get_stories(db_session, client):
     response = client.get("/v1/stories?metric_ids=NewBizDeals&metric_ids=CAC")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["count"] == 3
+    assert data["count"] == 7
 
     # Test multiple genre based filtering
-    response = client.get("/v1/stories?genre=GROWTH&genre=TRENDS")
+    response = client.get("/v1/stories/?genres=GROWTH&genres=TRENDS")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["count"] == len(stories)
+    assert data["count"] == 5
 
     # Test multiple grains based filtering
     response = client.get("/v1/stories?grains=day&grains=week")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["count"] == len(stories)
+    assert data["count"] == 4
 
     # Testing with multiple filters
     response = client.get("/v1/stories?grains=day&grains=week&genres=GROWTH&genres=TRENDS")
@@ -193,10 +253,45 @@ async def test_get_stories(db_session, client):
     response = client.get("/v1/stories?story_date=2022-01-01")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
-    assert data["count"] == 4
+    assert data["count"] == len(stories)
     assert data["results"][0]["story_date"] == "2020-01-01T00:00:00+0000"
 
+    response = client.get("/v1/stories?digest=PORTFOLIO&section=STATUS_CHANGES")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 1
+
+    response = client.get("/v1/stories?digest=PORTFOLIO&section=LIKELY_MISSES")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 1
+
+    response = client.get("/v1/stories?digest=PORTFOLIO&section=BIG_MOVES")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 1
+
+    response = client.get("/v1/stories?digest=PORTFOLIO&section=PROMISING_TRENDS")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 3
+
     response = client.get("/v1/stories?digest=PORTFOLIO&section=CONCERNING_TRENDS")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 1
+
+    response = client.get("/v1/stories?digest=METRIC&section=WHAT_IS_HAPPENING")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 5
+
+    response = client.get("/v1/stories?digest=METRIC&section=WHY_IS_IT_HAPPENING")
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+    assert data["count"] == 1
+
+    response = client.get("/v1/stories?digest=METRIC&section=WHAT_HAPPENS_NEXT")
     assert response.status_code == status.HTTP_200_OK
     data = response.json()
     assert data["count"] == 1
