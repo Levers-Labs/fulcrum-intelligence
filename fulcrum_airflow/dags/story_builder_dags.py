d57d84b3e6b5523dffa8770a96a142e6abb74adc
Prashant Tomar <prashant.tomar@velotio.com>
2024-08-28 16:56:21 +0530

    fixes for dag file

--- fulcrum_airflow/dags/story_builder_dags.py
+++ fulcrum_airflow/dags/story_builder_dags.py
@@ -1,24 +1,42 @@
+import os
 from datetime import datetime
 from typing import Any
 
 import requests
 from airflow.decorators import dag, task
 from airflow.models import Variable
-from airflow.operators.python import get_current_context
 from airflow.providers.amazon.aws.operators.ecs import EcsRunTaskOperator
+from airflow.providers.docker.operators.docker import DockerOperator
 
-# Environment variables
-DEFAULT_SCHEDULE = Variable.get("DEFAULT_SCHEDULE", "0 0 * * *")
-STORY_BUILDER_IMAGE = Variable.get("STORY_BUILDER_IMAGE", "story-builder-manager:latest")
-DOCKER_HOST = Variable.get("DOCKER_HOST")
-SERVER_HOST = Variable.get("SERVER_HOST", "http://localhost:8002")
-SECRET_KEY = Variable.get("SECRET_KEY")
-STORY_MANAGER_SERVER_HOST = Variable.get("STORY_MANAGER_SERVER_HOST")
-ANALYSIS_MANAGER_SERVER_HOST = Variable.get("ANALYSIS_MANAGER_SERVER_HOST")
-QUERY_MANAGER_SERVER_HOST = Variable.get("QUERY_MANAGER_SERVER_HOST")
-DSENSEI_BASE_URL = Variable.get("DSENSEI_BASE_URL")
-DATABASE_URL = Variable.get("DATABASE_URL")
-START_DATE = Variable.get("STORY_GENERATION_START_DATE")
+
+def get_config(key: str, default: str = "") -> str:
+    # Check if running locally or in production
+    if os.getenv("AIRFLOW_ENV") == "local":
+        return os.getenv(key, default)
+    return Variable.get(key, default)
+
+
+# Fetch environment-specific settings
+ENV = get_config("AIRFLOW_ENV")
+DEFAULT_SCHEDULE = get_config("DEFAULT_SCHEDULE", "0 0 * * *")
+STORY_BUILDER_IMAGE = get_config("STORY_BUILDER_IMAGE", "story-builder-manager:latest")
+DOCKER_HOST = get_config("DOCKER_HOST", "")
+SERVER_HOST = get_config("SERVER_HOST", "http://localhost:8002")
+SECRET_KEY = get_config("SECRET_KEY")
+STORY_MANAGER_SERVER_HOST = get_config("STORY_MANAGER_SERVER_HOST")
+ANALYSIS_MANAGER_SERVER_HOST = get_config("ANALYSIS_MANAGER_SERVER_HOST")
+QUERY_MANAGER_SERVER_HOST = get_config("QUERY_MANAGER_SERVER_HOST")
+DSENSEI_BASE_URL = get_config("DSENSEI_BASE_URL")
+DATABASE_URL = get_config("DATABASE_URL")
+START_DATE = get_config("STORY_GENERATION_START_DATE")
+AUTH0_API_AUDIENCE = get_config("STORY_GENERATION_START_DATE")
+AUTH0_ISSUER = get_config("AUTH0_ISSUER")
+AUTH0_CLIENT_SECRET = get_config("AUTH0_CLIENT_SECRET")
+AUTH0_CLIENT_ID = get_config("AUTH0_CLIENT_ID")
+ECS_SUBNETS = get_config("ECS_SUBNETS").split(" ")
+ECS_REGION = get_config("ECS_REGION")
+ECS_TASK_DEFINITION_NAME = get_config("ECS_TASK_DEFINITION_NAME")
+ECS_CLUSTER_NAME = get_config("ECS_CLUSTER_NAME")
 
 STORY_GROUP_META = {
     "GROWTH_RATES": {"schedule_interval": DEFAULT_SCHEDULE},
@@ -35,13 +53,13 @@ STORY_GROUP_META = {
 }
 
 
-def m2m_auth():
-    url = "https://insights-web-app.us.auth0.com/oauth/token"
+def fetch_auth_token():
+    url = f"{AUTH0_ISSUER.rstrip('/')}/oauth/token"
     headers = {"Content-Type": "application/json"}
 
     data = {
-        "client_id": "Ihu4GZuicrFp3mlnTmZJJMBKxVQIYQop",
-        "client_secret": "0i_-sIBz21ctAeWwwHs1wrCl-CpijNuJ5lUB3h55ocB5r4N7BjXtM1jkTNfduFkN",
+        "client_id": AUTH0_CLIENT_ID,
+        "client_secret": AUTH0_CLIENT_SECRET,
         "grant_type": "client_credentials",
     }
     response = requests.post(url, headers=headers, json=data, timeout=30)
@@ -49,11 +67,9 @@ def m2m_auth():
     return response_data["access_token"]
 
 
-headers = {"Authorization": f"Bearer {m2m_auth()}"}
+def fetch_all_metrics(auth_header: dict[str, str]) -> list[str]:
 
-
-def fetch_all_metrics() -> list[str]:
-    response = requests.get(f"{QUERY_MANAGER_SERVER_HOST.strip('/')}/metrics", headers=headers, timeout=30)
+    response = requests.get(f"{QUERY_MANAGER_SERVER_HOST.strip('/')}/metrics", headers=auth_header, timeout=30)
     response.raise_for_status()
     metrics_data = response.json()
     for metric in metrics_data.get("results"):
@@ -62,9 +78,9 @@ def fetch_all_metrics() -> list[str]:
     return [metric["id"] for metric in metrics_data.get("results", [])]
 
 
-def fetch_group_meta(group: str) -> dict[str, Any]:
+def fetch_group_meta(group: str, auth_header: dict[str, str]) -> dict[str, Any]:
     url = f"{STORY_MANAGER_SERVER_HOST.strip('/')}/stories/groups/{group}"
-    response = requests.get(url, headers=headers, timeout=20)
+    response = requests.get(url, headers=auth_header, timeout=20)
     response.raise_for_status()
     return response.json()
 
@@ -75,48 +91,73 @@ def create_story_group_dag(group: str, meta: dict[str, Any]) -> None:
 
     @dag(dag_id="story_group_dag", start_date=date_object, schedule_interval=meta["schedule_interval"], catchup=False)
     def story_group_dag():
+        @task(task_id="get_auth_header")
+        def get_auth_header():
+            return {"Authorization": f"Bearer {fetch_auth_token()}"}
+
         @task(task_id="fetch_metric_ids")
-        def fetch_metric_ids() -> list[str]:
-            return fetch_all_metrics()
+        def fetch_metric_ids(auth_header) -> list[str]:
+            return fetch_all_metrics(auth_header)
 
         @task(task_id="fetch_group_meta")
-        def fetch_group_meta_task() -> list[str]:
-            _meta = fetch_group_meta(group)  # Use a specific group or refactor as needed
+        def fetch_group_meta_task(auth_header) -> list[str]:
+            _meta = fetch_group_meta(group, auth_header)  # Use a specific group or refactor as needed
             return _meta.get("grains", [])
 
         @task(task_id="prepare_story_builder_commands")
         def prepare_story_builder_commands(_metrics: list[str], _grains: list[str]) -> list[str]:
             return [f"story generate {group} {metric_id} {grain}" for metric_id in _metrics for grain in _grains]
 
-        @task(task_id="run_ecs_tasks")
-        def run_ecs_tasks(commands: list[str]):
-            for idx, command in enumerate(commands):
-                EcsRunTaskOperator(
-                    task_id=f"run_ecs_task_{idx}",
-                    cluster="airflow-ecs-operator",
-                    task_definition="airflow-story-builder-task",
-                    launch_type="FARGATE",
-                    overrides={
-                        "containerOverrides": [
-                            {
-                                "name": "story-builder-manager",
-                                "command": command.split(" "),
-                            }
-                        ]
-                    },
-                    network_configuration={
-                        "awsvpcConfiguration": {
-                            "subnets": ["subnet-034a4d3dd0cccd058", "subnet-096d8d27095fad03c"],
-                            "assignPublicIp": "ENABLED",
-                        }
-                    },
-                    region_name="us-west-1",
-                ).execute(context=get_current_context())
-
-        metrics = fetch_metric_ids()
-        grains = fetch_group_meta_task()
+        auth_header = get_auth_header()
+        metrics = fetch_metric_ids(auth_header)
+        grains = fetch_group_meta_task(auth_header)
         commands = prepare_story_builder_commands(metrics, grains)
-        run_ecs_tasks(commands)
+
+        if ENV == "local":
+            DockerOperator.partial(
+                task_id="generate_stories",
+                image=STORY_BUILDER_IMAGE,
+                docker_url=DOCKER_HOST,
+                network_mode="bridge",
+                auto_remove="success",
+                environment={
+                    "SERVER_HOST": SERVER_HOST,
+                    "SECRET_KEY": SECRET_KEY,
+                    "ANALYSIS_MANAGER_SERVER_HOST": ANALYSIS_MANAGER_SERVER_HOST,
+                    "QUERY_MANAGER_SERVER_HOST": QUERY_MANAGER_SERVER_HOST,
+                    "DATABASE_URL": DATABASE_URL,
+                    "AUTH0_API_AUDIENCE": AUTH0_API_AUDIENCE,
+                    "AUTH0_ISSUER": AUTH0_ISSUER,
+                    "AUTH0_CLIENT_ID": AUTH0_CLIENT_ID,
+                    "AUTH0_CLIENT_SECRET": AUTH0_CLIENT_SECRET,
+                },
+            ).expand(command=commands)
+
+        else:
+
+            @task
+            def prepare_ecs_overrides(commands: list[str]) -> list[dict]:
+                # Prepare the ECS overrides based on the commands
+                return [
+                    {"containerOverrides": [{"name": "story-builder-manager", "command": command.split(" ")}]}
+                    for command in commands
+                ]
+
+            ecs_overrides = prepare_ecs_overrides(commands)
+
+            EcsRunTaskOperator.partial(
+                task_id="run_ecs_task",
+                cluster=ECS_CLUSTER_NAME,
+                task_definition=ECS_TASK_DEFINITION_NAME,
+                launch_type="FARGATE",
+                network_configuration={
+                    "awsvpcConfiguration": {
+                        "subnets": ECS_SUBNETS,
+                        "assignPublicIp": "ENABLED",
+                    }
+                },
+                region_name=ECS_REGION,
+            ).expand(overrides=ecs_overrides)
 
     story_group_dag()
 
