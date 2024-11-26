87b9c3804d3430a5feace0c06d53ff7b39135cc0
sadaf-vl <sadaf.shaikh@velotio.com>
2024-11-26 11:16:37 +0530

    updated update tenant config APIs

--- insights_backend/insights_backend/core/routes.py
+++ insights_backend/insights_backend/core/routes.py
@@ -193,7 +193,7 @@ async def update_tenant_config(
     """
     try:
         # Attempt to update the tenant configuration
-        updated_config = await tenant_crud_client.update_tenant_config(tenant_id, config)
+        updated_config = await tenant_crud_client.update_tenant_config(tenant_id, config)  # type: ignore
         return updated_config
     except NotFoundError as e:
         # Raise an HTTPException if the tenant is not found
