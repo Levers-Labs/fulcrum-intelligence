e29c6e363735678bd11e18f932463ba6be28c44f
sadaf-vl <sadaf.shaikh@velotio.com>
2025-03-18 17:39:09 +0530

    updated packages

--- analysis_manager/lambda.py
+++
@@ -1,9 +0,0 @@
-"""
-This file is used to deploy the FastAPI app as a Lambda function using AWS API Gateway.
-"""
-
-from mangum import Mangum
-
-from analysis_manager.main import app
-
-handler = Mangum(app)
